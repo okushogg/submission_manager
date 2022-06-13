4280fae66c047138b49ac37b348d06e436c6abb3
Shota Okugawa <okusho721@gmail.com>
2022-06-13 08:50:29 +0700

    6/13 fix undefined index password

--- join/index.php
+++ join/index.php
@@ -2,17 +2,17 @@
 session_start();
 require('../libs.php');
 
-if(isset($_GET['action']) && isset($_SESSION['form'])){
+if (isset($_GET['action']) && isset($_SESSION['form'])) {
   $form = $_SESSION['form'];
 } else {
   $form = [
-      'first_name' => '',
-      'last_name' => '',
-      'grade' => 0,
-      'class' => 0,
-      'email' => '',
-      'password' => '',
-      'is_active' => true
+    'first_name' => '',
+    'last_name' => '',
+    'grade' => 0,
+    'class' => 0,
+    'email' => '',
+    'password' => '',
+    'is_active' => true
   ];
 }
 
@@ -23,50 +23,68 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // 姓名の確認
   $form['first_name'] = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_STRING);
   if ($form['first_name'] === '') {
-      $error['first_name'] = 'blank';
+    $error['first_name'] = 'blank';
   }
 
   $form['last_name'] = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_STRING);
   if ($form['last_name'] === '') {
-      $error['last_name'] = 'blank';
+    $error['last_name'] = 'blank';
   }
 
   // Emailのチェック
   $form['email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
   if ($form['email'] === '') {
-      $error['email'] = 'blank';
+    $error['email'] = 'blank';
   } else {
-      $db = dbconnect();
-      $stmt = $db->prepare('select count(*) from students where email=?');
-      if(!$stmt) {
-          die($db->error);
-      }
-      $stmt->bind_param('s', $form['email']);
-      $success = $stmt->execute();
-      if(!$success){
-          die($db->error);
-      }
+    $db = dbconnect();
+    $stmt = $db->prepare('select count(*) from students where email=?');
+    if (!$stmt) {
+      die($db->error);
+    }
+    $stmt->bind_param('s', $form['email']);
+    $success = $stmt->execute();
+    if (!$success) {
+      die($db->error);
+    }
 
-      $stmt->bind_result($cnt);
-      $stmt->fetch();
+    $stmt->bind_result($cnt);
+    $stmt->fetch();
   }
 
   // パスワードのチェック
   $form['password'] = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
   if ($form['password'] === '') {
-      $error['password'] = 'blank';
+    $error['password'] = 'blank';
   } elseif (strlen($form['password']) < 4) {
-      $error['password'] = 'length';
+    $error['password'] = 'length';
   }
 
   // 学年・クラスのチェック
   $form['grade'] = filter_input(INPUT_POST, 'grade', FILTER_SANITIZE_NUMBER_INT);
-  if($form['grade'] === 0){
+  if ($form['grade'] == 0) {
     $error['grade'] = 'blank';
   }
   $form['class'] = filter_input(INPUT_POST, 'class', FILTER_SANITIZE_NUMBER_INT);
-  if($form['class'] === 0){
+  if ($form['class'] == 0) {
     $error['class'] = 'blank';
+  }
+
+  if (empty($error)) {
+    $_SESSION['form'] = $form;
+
+    // 画像のアップロード
+    if ($image['name'] !== '') {
+      $filename = date('Ymdhis') . '_' . $image['name'];
+      if (!move_uploaded_file($image['tmp_name'], '../student_picture/' . $filename)) {
+        die('ファイルのアップロードに失敗しました');
+      }
+      $_SESSION['form']['image'] = $filename;
+    } else {
+      $_SESSION['form']['image'] = '';
+    }
+
+    header('Location: check.php');
+    exit();
   }
 }
 ?>
@@ -94,16 +112,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form action="" method="post" enctype="multipart/form-data">
           <dl>
             <dt>姓<span class="required">（必須）</span></dt>
+            <?php if (isset($error['last_name']) && $error['first_name'] === 'blank') : ?>
+              <p class="error">* 苗字を入力してください</p>
+            <?php endif; ?>
             <dd>
               <input type="text" name="last_name" size="35" maxlength="255" value="<?php echo h($form['last_name']); ?>" />
             </dd>
 
             <dt>名<span class="required">（必須）</span></dt>
+            <?php if (isset($error['first_name']) && $error['first_name'] === 'blank') : ?>
+              <p class="error">* 名前を入力してください</p>
+            <?php endif; ?>
             <dd>
               <input type="text" name="first_name" size="35" maxlength="255" value="<?php echo h($form['first_name']); ?>" />
             </dd>
 
             <dt>学年<span class="required">（必須）</span></dt>
+            <?php if (isset($error['grade']) && $error['grade'] === 'blank') : ?>
+              <p class="error">* 学年を入力してください</p>
+            <?php endif; ?>
             <dd>
               <select name="grade">
                 <option value="0">-</option>
@@ -114,22 +141,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </dd>
 
             <dt>クラス<span class="required">（必須）</span></dt>
+            <?php if (isset($error['class']) && $error['class'] === 'blank') : ?>
+              <p class="error">* クラスを入力してください</p>
+            <?php endif; ?>
             <dd>
               <select name="class">
                 <option value="0">-</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
+                <option value="1">A</option>
+                <option value="2">B</option>
+                <option value="3">C</option>
               </select>
             </dd>
 
             <dt>メールアドレス<span class="required">（必須）</span></dt>
+            <?php if (isset($error['email']) && $error['email'] === 'blank') : ?>
+              <p class="error">* メールアドレスを入力してください</p>
+            <?php endif; ?>
             <dd>
-              <input type="text" name="email" size="35" maxlength="255" value="" />
+              <input type="text" name="email" size="35" maxlength="255" value="<?php echo h($form['email']); ?>" />
 
             <dt>パスワード<span class="required">（必須）</span></dt>
             <dd>
-              <input type="password" name="password" size="10" maxlength="20" value="" />
+              <input type="password" name="password" size="10" maxlength="20" value="<?php echo h($form['password']); ?>" />
+            </dd>
+            <?php if (isset($error['password']) && $error['password'] === 'blank') : ?>
+              <p class="error">* パスワードを入力してください</p>
+            <?php elseif (isset($error['password']) && $error['password'] === 'length') : ?>
+              <p class="error">* パスワードは4文字以上で入力してください</p>
+            <?php endif; ?>
+
+            <dt>写真など</dt>
+            <dd>
+              <input type="file" name="image" size="35" value="" />
+              <?php if (isset($error['image']) && $error['image'] === 'type') : ?>
+                <p class="error">* 写真などは「.png」または「.jpg」の画像を指定してください</p>
+                <p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
+              <?php endif; ?>
             </dd>
 
             <dd>
