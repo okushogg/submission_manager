8adefbfdf458d8d2b3d86780dac8655249df9fdc
Shota Okugawa <okusho721@gmail.com>
2022-07-10 17:27:50 +0700

    Try js again

--- submission.js
+++ submission.js
@@ -3,12 +3,12 @@ function addForm($classes_info) {
   var select_class = document.createElement('select');
   select_class.id = 'class';
   select_class.name = 'class_id';
-  // for(var i = 0; i<$classes_info.length; i++) {
-  //   let op = document.createElement("option");
-  //   op.value = $classes_info[i].id;
-  //   op.text = $classes_info[i].grade;
-  //   document.getElementById('class').appendChild(op);
-  // }
+  for(var i = 0; i<$classes_info.length; i++) {
+    let op = document.createElement("option");
+    op.value = $classes_info[i].id;
+    op.text = $classes_info[i].grade;
+    document.getElementById('class').appendChild(op);
+  }
   var parent = document.getElementById('class_select');
   parent.appendChild(select_class);
   i++ ;
