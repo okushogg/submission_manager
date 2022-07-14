45808d0bcc82fee396e0907968e17d62e1a5d5d7
Shota Okugawa <okusho721@gmail.com>
2022-07-14 16:29:33 +0700

    Smarty setting

--- sample.php
+++ sample.php
@@ -1,15 +1,12 @@
 <?php
 
-require_once('smarty/Smarty.class.php');
+require('smarty/Smarty.class.php');
 
 $smarty = new Smarty();
 
-$smarty->template_dir = '/Applications/MAMP/htdocs/submission_manager/templates/';
-$smarty->compile_dir  = '~/submission_manager/templates_c/';
-$smarty->config_dir   = '~/submission_manager/configs/';
-$smarty->cache_dir    = '~/submission_manager/cache/';
+$smarty->template_dir = 'templates/';
+$smarty->compile_dir  = 'templates_c/';
 
-$smarty->assign('msg','Hello World!');
+$smarty->assign('msg', 'Fucking stuck');
 $smarty->display('sample.tpl');
-
 ?>
---
+++ templates_c/acba3f9e92224919e51226624c69713dc4e72edb_0.file.sample.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/* Smarty version 4.1.1, created on 2022-07-13 04:19:56
+  from '/Applications/MAMP/htdocs/submissions_manager/templates/sample.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.1.1',
+  'unifunc' => 'content_62ce47ec500f91_06821696',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'acba3f9e92224919e51226624c69713dc4e72edb' => 
+    array (
+      0 => '/Applications/MAMP/htdocs/submissions_manager/templates/sample.tpl',
+      1 => 1657685730,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_62ce47ec500f91_06821696 (Smarty_Internal_Template $_smarty_tpl) {
+?><html>
+<head>
+<title>Smarty Test</title>
+</head>
+<body>
+
+<h1><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+</h2>
+
+</body>
+</html><?php }
+}
