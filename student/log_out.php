be5dcaada0eda02feba46781582a10d6813594cc
Shota Okugawa <okusho721@gmail.com>
2022-07-22 14:08:53 +0700

    fix logout error caused by session login

--- student/log_out.php
+++ student/log_out.php
@@ -5,5 +5,6 @@ unset($_SESSION['auth']['teacher_id']);
 unset($_SESSION['auth']['last_name']);
 unset($_SESSION['auth']['first_name']);
 unset($_SESSION['auth']['student_image_id']);
+unset($_SESSION['auth']['login']);
 header('Location: log_in.php');
 exit();
--- teacher/log_out.php
+++ teacher/log_out.php
@@ -5,6 +5,7 @@ unset($_SESSION['auth']['student_id']);
 unset($_SESSION['auth']['last_name']);
 unset($_SESSION['auth']['first_name']);
 unset($_SESSION['auth']['teacher_image_id']);
+unset($_SESSION['auth']['login']);
 header('Location: log_in.php');
 exit();
 ?>
