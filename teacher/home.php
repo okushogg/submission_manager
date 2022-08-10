45b4da8dc744f30630ca0bbe33cfb6bef8896ac2
Shota Okugawa <okusho721@gmail.com>
2022-08-10 22:04:59 +0700

    teacher/home.php moved code about DB to each model

--- teacher/home.php
+++ teacher/home.php
@@ -4,7 +4,15 @@ require('../private/libs.php');
 require('../private/dbconnect.php');
 
 require_once('../private/set_up.php');
+require_once('../model/teachers.php');
+require_once('../model/images.php');
+require_once('../model/classes.php');
+
+
 $smarty = new Smarty_submission_manager();
+$teacher = new teacher();
+$image = new image();
+$class = new ClassRoom();
 
 $form = [
   "last_name" => '',
@@ -33,17 +41,14 @@ $teacher_id = is_teacher_login();
 $image_id = $_SESSION['auth']['teacher_image_id'];
 
 // 画像の情報を取得
-$pic_info = get_pic_info($db, $image_id);
+$pic_info = $image->get_pic_info($db, $image_id);
 $smarty->assign('pic_info', $pic_info);
 
 
 // 今年度のクラスを取得する
-$classes_stmt = $db->prepare("select id, year, grade, class from classes where year=:year");
-$classes_stmt->bindParam(':year', $this_year, PDO::PARAM_STR);
-$classes_stmt->execute();
-$classes_info = $classes_stmt->fetchAll(PDO::FETCH_ASSOC);
+$classes_info = $class->get_this_year_classes($db, $this_year);
 $smarty->assign('classes_info', $classes_info);
-$cnt = count($classes_info);
+
 
 // 学年ごとにクラスのデータを配列で取得
 $classes_array = get_classes($classes_info);
